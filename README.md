0a78abc49c8b7832db74833c2b4d998cab1c4d5b
Ray Strods <strods.r@gmail.com>
2019-11-14 12:36:03 -0800

    Climbing App First Deploy

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # climbing-app
 Streamlit 
+
+# Required Files
+1 setup.sh
+2 Procfile
+3 requirements.txt
---
+++ climbing_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    '''My Climbing App'''
+    
+    st.title('Hello World')
+    
+if __name__ == '__main__':
+    main()
